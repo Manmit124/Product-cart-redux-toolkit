6058982130041c83e093b69dbb65d6912dcfa1a4
Manmit Tiwade <112580293+Manmit124@users.noreply.github.com>
2023-07-10 00:07:23 +0530

    this is my first commit for the product cart code

---
+++ src/Components/Home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+const img2 =
+  "https://cdn.shopify.com/s/files/1/2428/5565/products/Neemans-HaleBlack-ReLive-Knits-Jogger-FrontRightLogo-Comfortable-Shoes_1024x.jpg?v=1662876260";
+
+const Home = () => {
+  const ProductList = [
+    {
+      name: "mac book",
+      price: 12000,
+      imgSrc: img1,
+      id: "asdasdfasdf",
+    },
+    {
+      name: "Black shoes",
+      price: 500,
+      imgSrc: img2,
+      id: "asdasdtfasdf",
+    },
+  ];
+
+  const dispatch = useDispatch();
+  const addtocartHandler = (options) => {
+    console.log(options);
+    dispatch({
+      type: "calculatePrice",
+    });
+    dispatch({ type: "addToCart", payload: options });
+    toast.success("Added to cart");
+  };
+  return (
+    <div className="home">
+      {ProductList.map((i) => (
+        <ProductCard
+          key={i.id}
+          imgSrc={i.imgSrc}
+          name={i.name}
+          price={i.price}
+          id={i.id}
+          handler={addtocartHandler}
+        />
+      ))}
+    </div>
+  );
+};
+
+const ProductCard = ({ name, id, price, handler, imgSrc }) => (
+  <div className="productCard">
+    <img src={imgSrc} alt={name} />
+    <p  style={{color:'black'}}>{name}</p>
+    <h4 style={{color:'black',fontWeight:'bold'}}>${price}</h4>
+    <button onClick={() => handler({ name, price, id, quantity: 1, imgSrc })}>
+      Add to Cart
+    </button>
+  </div>
+);
+
+export default Home;
